8849d0eb526efda52f2540f502525c8b7df476c2
webnikler <webnikler@gmail.com>
2024-12-07 21:00:39 +0300

    add solution for 2-module 1-task

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
@@ -3,29 +3,50 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { Task } from "./task.model";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {}
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 
   @Get(":id")
-  getTaskById(@Param("id") id: string) {}
+  getTaskById(@Param("id") id: string) {
+    const res = this.tasksService.getTaskById(id);
+
+    if (!res) throw new NotFoundException();
+    return res;
+  }
 
   @Post()
-  createTask(@Body() task: Task) {}
+  createTask(@Body() task: CreateTaskDto) {
+    return this.tasksService.createTask(task);
+  }
 
   @Patch(":id")
-  updateTask(@Param("id") id: string, @Body() task: Task) {}
+  updateTask(@Param("id") id: string, @Body() task: Task) {
+    const res = this.tasksService.updateTask(id, task);
+
+    if (!res) throw new NotFoundException();
+    return res;
+  }
 
   @Delete(":id")
-  deleteTask(@Param("id") id: string) {}
+  deleteTask(@Param("id") id: string) {
+    const res = this.tasksService.deleteTask(id);
+
+    if (!res) throw new NotFoundException();
+    return res;
+  }
 }
